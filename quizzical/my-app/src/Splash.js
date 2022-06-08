1930e38a6e3eb65a50900ee04c981c622d2c63c7
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-06-08 15:04:28 +0200

    Create splash component

---
+++ quizzical/my-app/src/Splash.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Splash() {
+    return (
+        <>
+            <h1>Quizzical</h1>
+            <h3>An React API based Quiz Program</h3>
+            <button>Start Quiz</button>
+        </>
+    )
+}
+
+export default Splash
